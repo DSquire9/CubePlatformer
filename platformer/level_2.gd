6983fdcebc1c5c8c411463c4bf6e01890d36164e
DSquire9 <dss9593@rit.edu>
2025-01-23 18:07:18 -0500

    Created level2

---
+++ platformer/level_2.gd
@@ -0,0 +1,25 @@
+extends Node
+
+@export var next_level = "res://.tscn"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$DeathScreen.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+		
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $DeathScreen.visible:
+		get_tree().reload_current_scene()
+
+
+func _on_player_hit() -> void:
+	$DeathScreen.show()
+	
+func _on_player_clear() -> void:
+	get_tree().change_scene_to_file(next_level)
